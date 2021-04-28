1d100e272512d14787d3384c219dccb17689ac20
Viper25 <viper25@users.noreply.github.com>
2021-04-28 08:25:39 +0800

    Add shell script for cronjobs

---
+++ member_contribution.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd /home/vibinjk/stosc_xero_jobs
+source .env/bin/activate
+
+# virtualenv is now active, which means your PATH has been modified.
+# Don't try to run python from /usr/bin/python, just run "python" and
+# let the PATH figure out which version to run (based on what your
+# virtualenv has configured).
+python member_contribution.py
