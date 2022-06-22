cf133f540a45cde0b19becd0a21288838dd5db0f
viper25 <327990+viper25@users.noreply.github.com>
2022-06-22 17:41:09 +0800

    Add member ID to output

--- member_contribution.py
+++ member_contribution.py
@@ -29,7 +29,7 @@ init_colorit()
 # VARIABLES & CONFIGURATION
 
 since_date = datetime.now().strftime("%Y-01-01")
-since_date = '2021-01-01'
+# since_date = '2021-01-01'
 
 bank_accounts = {"DBS": "1000", "NETS": "1001", "Cash": "1002"}
 receive_txns = []
@@ -104,6 +104,8 @@ accounts_of_interest = [
     {"AccountCode": "3240", "AccountName": "Migrant Workers", "keyword": "Migrant", "Total": 0}
 ]
 
+df_members = pd.read_csv("csv\\xero_contacts.csv")
+
 write_to_csv = False
 # ===============================================================
 
@@ -157,12 +159,19 @@ def cleanup_txns_df(_df_tnxs):
 
     return _df_tnxs
 
+def get_member_ID(_str_name):
+    # Get the member ID from the name
+    if len(df_members.loc[df_members['ContactID'] == _str_name]) == 0:
+        _member_code = 'NA'
+    else:
+        _member_code = df_members.loc[df_members['ContactID'] == _str_name].iloc[0][0]
+    return _member_code
+
 
 # https://api-explorer.xero.com/accounting/banktransactions/getbanktransactions?query-where=BankAccount.Code%3D%3D%221000%22&query-page=1&header-if-modified-since=2021-04-20
 def get_member_txns(since_date):
 
     print(color(f"\nProcessing Member Transactions\n================", Colors.blue))
-    _member_txns = {}
     for bank_account in bank_accounts.items():
         # Reset page counter for each account (DBS, NETS etc.)
         has_more_pages = True
@@ -185,6 +194,7 @@ def get_member_txns(since_date):
                         # Build the output item
                         "ContactID": _txn["Contact"]["ContactID"],
                         "ContactName": _txn["Contact"]["Name"],
+                        "MemberID": get_member_ID(_txn["Contact"]["ContactID"]),
                         "BankAccount": _txn["BankAccount"]["Name"],
                         # "Year": _txn['DateString'].split('-')[0],
                         "Year": str(utils.parse_Xero_Date(_txn["Date"]).year),
@@ -216,6 +226,7 @@ def get_member_txns(since_date):
                             # Build the output item
                             "ContactID": _txn["Contact"]["ContactID"],
                             "ContactName": _txn["Contact"]["Name"],
+                            "MemberID": get_member_ID(_txn["Contact"]["ContactID"]),
                             "BankAccount": _txn["BankAccount"]["Name"],
                             # "Year": _txn['DateString'].split('-')[0],
                             "Year": str(utils.parse_Xero_Date(_txn["Date"]).year),
@@ -260,6 +271,7 @@ def get_member_invoice_payments(since_date):
                     # Build the output item
                     "ContactID": _payments["Invoice"]["Contact"]["ContactID"],
                     "ContactName": _payments["Invoice"]["Contact"]["Name"],
+                    "MemberID": get_member_ID(_payments["Invoice"]["Contact"]["ContactID"]),
                     # We assume any invoices not starting with INV is issued for harvest Festival
                     "AccountCode": "3010" if _payments["Invoice"]["InvoiceNumber"].startswith("INV") else "3200",
                     # "Year": _txn['DateString'].split('-')[0],
@@ -301,7 +313,7 @@ if write_to_csv:
     df_merged.to_csv("csv\member_contributions.csv", index=False)
 
 # Group by Contacts to show all payments from a member
-df_grouped = df_merged.groupby(["ContactID", "ContactName", "AccountCode", "Account", "Year"]).sum().reset_index()
+df_grouped = df_merged.groupby(["ContactID", "ContactName", "MemberID", "AccountCode", "Account", "Year"]).sum().reset_index()
 print(color(df_grouped.sort_values(by=["ContactName"]).head(5), (200, 200, 200)))
 if write_to_csv:
     df_grouped.to_csv("csv\member_contributions_grouped.csv", index=True)
@@ -310,7 +322,7 @@ if write_to_csv:
 # Ref: https://pbpython.com/pandas-pivot-table-explained.html
 # Pivot to show all Accounts in cols
 # The values column automatically averages the data so should change to sum.
-df_pivoted = df_grouped.pivot_table(index="ContactName", columns="Account", values="LineAmount", aggfunc=np.sum, fill_value=0)
+df_pivoted = df_grouped.pivot_table(index=["ContactName","Year","MemberID"], columns="Account", values="LineAmount", aggfunc=np.sum, fill_value=0)
 if write_to_csv:
     df_pivoted.to_csv("csv\member_contributions_pivoted.csv", index=True)
 
