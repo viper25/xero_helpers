f750df531c1b0a24728b32587fded7160b3e276f
viper25 <327990+viper25@users.noreply.github.com>
2023-02-23 09:58:17 +0800

    Update tracking_categories.sh

--- jobs/tracking_categories.sh
+++ jobs/tracking_categories.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /home/azureuser/jobs/stosc_xero_tracked_accounts
-source .env/bin/activate
+source .venv/bin/activate
 
 # virtualenv is now active, which means your PATH has been modified.
 # Don't try to run python from /usr/bin/python, just run "python" and
