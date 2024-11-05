b31e500632faf13036d8b7d489d7b401fcaed286
viper25 <327990+viper25@users.noreply.github.com>
2024-11-05 14:57:21 +0800

    Update warning for regex and formatting

--- jobs/stosc_xero_outstandings/outstandings.py
+++ jobs/stosc_xero_outstandings/outstandings.py
@@ -5,9 +5,12 @@
 as of a certain date
 """
 
-import utils
-from colorama import Fore, init
+import pickle
+
 import pandas as pd
+from colorama import Fore, init
+
+import utils
 
 init(autoreset=True)
 # initialize empty dataframe
@@ -18,7 +21,8 @@ page = 0
 # Go through pages (100 txns per page)
 while has_more_pages:
     page += 1
-    invoices = utils.xero_get(f"https://api.xero.com/api.xro/2.0/Invoices?Statuses=AUTHORISED,PAID&where=Type%3D%22ACCREC%22&summaryOnly=true&page={page}")
+    invoices = utils.xero_get(
+        f"https://api.xero.com/api.xro/2.0/Invoices?Statuses=AUTHORISED,PAID&where=Type%3D%22ACCREC%22&summaryOnly=true&page={page}")
     invoice_list = invoices['Invoices']
     if len(invoices['Invoices']) == 0:
         has_more_pages = False
@@ -36,31 +40,31 @@ while has_more_pages:
             except ValueError as e:
                 if e.args[0] == 'not enough values to unpack (expected 3, got 2)':
                     # Special Handling: a couple of invoices of type INV-0003 were issued in year 2020
-                    if invoice['InvoiceNumber']=='INV-0003':
+                    if invoice['InvoiceNumber'] == 'INV-0003':
                         year = "2019"
-                    elif invoice['InvoiceNumber']=='INV-0007':
+                    elif invoice['InvoiceNumber'] == 'INV-0007':
                         year = "2019"
                     else:
                         raise e
                 else:
                     raise e
 
-            if invoice['InvoiceNumber']=='INV-0003':
+            if invoice['InvoiceNumber'] == 'INV-0003':
                 temp_df = pd.DataFrame([{'Inv': 'S-19', 'Total': invoice['Total'], 'Due': invoice['AmountDue']}])
-                df = pd.concat([df,temp_df], ignore_index=True)
-            elif invoice['InvoiceNumber']=='INV-0007':
+                df = pd.concat([df, temp_df], ignore_index=True)
+            elif invoice['InvoiceNumber'] == 'INV-0007':
                 temp_df = pd.DataFrame([{'Inv': 'S-19', 'Total': invoice['Total'], 'Due': invoice['AmountDue']}])
-                df = pd.concat([df,temp_df], ignore_index=True)
+                df = pd.concat([df, temp_df], ignore_index=True)
             elif invoice['InvoiceNumber'].startswith('INV'):
                 # Subscriptions
                 x = f"S-{invoice['InvoiceNumber'].split('-')[1]}"
                 temp_df = pd.DataFrame([{'Inv': x, 'Total': invoice['Total'], 'Due': invoice['AmountDue']}])
-                df = pd.concat([df,temp_df], ignore_index=True)
+                df = pd.concat([df, temp_df], ignore_index=True)
             elif invoice['InvoiceNumber'].startswith('HF-'):
                 # Harvest Festival Invoice
                 x = f"H-{invoice['InvoiceNumber'].split('-')[1]}"
                 temp_df = pd.DataFrame([{'Inv': x, 'Total': invoice['Total'], 'Due': invoice['AmountDue']}])
-                df = pd.concat([df,temp_df], ignore_index=True)            
+                df = pd.concat([df, temp_df], ignore_index=True)
             else:
                 raise Exception
 
@@ -70,7 +74,7 @@ df_grouped = df.groupby('Inv').sum()
 # df_grouped['%'] = df_grouped['%'].apply(lambda x: '{:.1f}%'.format(x))
 
 # Remove rows that have the "Due" column = 0
-df_grouped = df_grouped[df_grouped['Due']!=0]
+df_grouped = df_grouped[df_grouped['Due'] != 0]
 
 df_grouped['Total'] = df_grouped['Total'].apply(lambda x: '$ {:,.0f}'.format(x))
 df_grouped['Due'] = df_grouped['Due'].apply(lambda x: '$ {:,.0f}'.format(x))
@@ -78,7 +82,7 @@ df_grouped['Due'] = df_grouped['Due'].apply(lambda x: '$ {:,.0f}'.format(x))
 # Reset the index
 df_reset = df_grouped.reset_index()
 # Extract the year from the index column
-df_reset['year'] = df_reset.iloc[:, 0].str.extract('(\d+)').astype(int)
+df_reset['year'] = df_reset.iloc[:, 0].str.extract(r'(\d+)').astype(int)
 # Sort the dataframe based on the extracted year
 df_sorted = df_reset.sort_values(by='year')
 # Set the index back to the original column and drop the year column
@@ -86,8 +90,7 @@ df_sorted = df_sorted.set_index(df_sorted.columns[0]).drop('year', axis=1)
 
 print(f"\n{Fore.WHITE}{df_sorted.to_string()}\n")
 
-import pickle
-filehandler = open(b"outstandings.pickle","wb")
-pickle.dump(df_sorted,filehandler)
+filehandler = open(b"outstandings.pickle", "wb")
+pickle.dump(df_sorted, filehandler)
 filehandler.close()
 print("Done")
